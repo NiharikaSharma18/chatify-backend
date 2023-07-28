0bad790b85a6adee20823442aa351e48b4e72343
harry <nihar@ni.com>
2023-07-29 00:11:22 +0530

    commit done

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
+app.get("/",async(req,res) => {
+return res.status(200).json({
+  message:"welocme to api"
+})
+})
+
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body; 
